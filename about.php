32623c6020a24a0069355d1f0cb285567dda3f24
Arcanae <justin.courdesse@gmail.com>
2017-05-30 15:23:28 +0200

    page created

---
+++ about.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inspiration | Creative Boom</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/icone.png" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+    <?php
+        include("header.php");
+    ?>
+
+    <main>
+                <h1 class="page__title">About</h1>
+        <h2>Launched in 2009, Creative Boom is an online magazine that celebrates, inspires and supports the creative community.</h2>
+
+<p>From graduates, creative professionals and freelancers to independent artists, designers and makers, our audience of half a million monthly readers spans the globe from America to Australia, each using our tips, resources and inspiration to succeed.</p>
+
+<p>With a focus on art, crafts, graphic design, illustration, photography and travel, Creative Boom showcases emerging and established talent, shares recommended resources such as the latest gadgets and books, interviews the brightest and best creatives, delves into people's inspiring workspaces and offers invaluable tips and insight to help you at every stage of your creative career – that's whether you've just graduated, started a new job or launched a new business.</p>
+
+<p>Featured in The Guardian, The Independent, Swiss Miss, Colossal, My Modern Met, Design Boom, Design Taxi and Fubiz, Creative Boom is your dedicated daily fix for creative tips, inspiration and ideas.</p>
+
+<h3>Our Editorial Team</h3>
+
+<div class="two-col">
+
+  <div class="col">
+
+    <p><strong>Katy Cowan – Founding Editor</strong><br>
+    Twitter: <a href="http://www.twitter.com/katylcowan">@katylcowan</a><br>
+     Email: <a href="/cdn-cgi/l/email-protection#7d161c09043d1e0f181c09140b181f121210531e1210"><span class="__cf_email__" data-cfemail="a8c3c9dcd1e8cbdacdc9dcc1decdcac7c7c586cbc7c5">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a></p>
+
+    <p><strong>Laura Collinson – Assistant Editor</strong><br>
+    Twitter: <a href="https://twitter.com/Lozcoll">@lozcoll</a><br>
+    Email: <a href="/cdn-cgi/l/email-protection#610d00141300210213040015081704030e0e0c4f020e0c"><span class="__cf_email__" data-cfemail="ee828f9b9c8fae8d9c8b8f9a87988b8c818183c08d8183">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a></p>
+
+    <p><strong>Anthony Wood – Writer</strong><br>
+    Twitter: <a href="https://twitter.com/twonwood">@twonwood</a><br>
+    Email: <a href="/cdn-cgi/l/email-protection#fe9f908a96919087be9d8c9b9f8a97889b9c919193d09d9193"><span class="__cf_email__" data-cfemail="3958574d51565740795a4b5c584d504f5c5b565654175a5654">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>  </p>
+
+    <p><strong>Darren Clanford – Writer</strong><br>
+    Twitter: <a href="https://twitter.com/123dmc">@123dmc</a><br>
+    Email: <a href="/cdn-cgi/l/email-protection#cca8adbebea9a28cafbea9adb8a5baa9aea3a3a1e2afa3a1"><span class="__cf_email__" data-cfemail="1276736060777c5271607773667b6477707d7d7f3c717d7f">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>  </p>
+
+    <p><strong>Emily Gosling – Writer</strong><br>
+    Twitter: <a href="https://twitter.com/nalascarlett">@nalascarlett</a><br>
+    Email: <a href="/cdn-cgi/l/email-protection#d7b2babebbae97b4a5b2b6a3bea1b2b5b8b8baf9b4b8ba"><span class="__cf_email__" data-cfemail="2a4f474346536a49584f4b5e435c4f4845454704494547">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>  </p>
+  </div>
+
+  <div class="col">
+
+    <p><strong>Glenn Hickling – Writer</strong><br>
+Twitter: <a href="https://twitter.com/hicksbomb">@hicksbomb</a><br>
+Email: <a href="/cdn-cgi/l/email-protection#26414a4348486645544347524f50434449494b0845494b"><span class="__cf_email__" data-cfemail="4e29222b20200e2d3c2b2f3a27382b2c212123602d2123">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>  </p>
+
+    <p><strong>Leyla Muratovic – Writer</strong><br>
+Twitter: <a href="https://twitter.com/shillington_">@shillington_</a><br>
+Email: <a href="/cdn-cgi/l/email-protection#ff939a86939ebf9c8d9a9e8b96899a9d909092d19c9092"><span class="__cf_email__" data-cfemail="b8d4ddc1d4d9f8dbcaddd9ccd1cedddad7d7d596dbd7d5">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a></p>
+
+    <p><strong>Sara Khan – Writer</strong><br>
+Twitter: <a href="https://twitter.com/wheres_sk">@wheres_sk</a><br>
+Email: <a href="/cdn-cgi/l/email-protection#2e5d4f5c4f6e4d5c4b4f5a47584b4c414143004d4143"><span class="__cf_email__" data-cfemail="5a293b283b1a39283f3b2e332c3f3835353774393537">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>  </p>
+
+    <p><strong>Sarah McHugh – Writer</strong><br>
+Twitter: <a href="https://twitter.com/sarahmchugh">@sarahmchugh</a><br>
+Email: <a href="/cdn-cgi/l/email-protection#a7d4c6d5c6cfe7c4d5c2c6d3ced1c2c5c8c8ca89c4c8ca"><span class="__cf_email__" data-cfemail="33405241525b7350415652475a4556515c5c5e1d505c5e">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>  </p>
+
+    <p><strong>Tom May – Writer</strong><br>
+Twitter: <a href="https://twitter.com/tom_may">@tom_may</a><br>
+Email: <a href="/cdn-cgi/l/email-protection#c1b5aeac81a2b3a4a0b5a8b7a4a3aeaeacefa2aeac"><span class="__cf_email__" data-cfemail="4f3b20220f2c3d2a2e3b26392a2d202022612c2022">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a></p>
+
+  </div>
+
+</div>
+    </main>
+
+<?php
+        include("footer.php");
+    ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+    <header class="bigheader">
+        <div class="logo">
+            <a href="#"><img src="images/logo.svg" alt="logo"></a>
+        </div>
+        <nav>
+            <ul>
+                <li class="menu">ABOUT </li>
+                <li class="menu">INSPIRATION ▾
+                    <ul>
+                        <li><a href="#">Art</a></li>
+                        <li><a href="#">Crafts</a></li>
+                        <li><a href="#">Graphic Design</a></li>
+                        <li><a href="#">Illustration</a></li>
+                        <li><a href="#">Photography</a></li>
+                        <li><a href="#">Travel</a></li>
+                    </ul>
+                </li>
+                <li class="menu">TIPS ▾
+                    <ul>
+                        <li><a href="#">Career</a></li>
+                        <li><a href="#">Freelancing</a></li>
+                        <li><a href="#">Marketing</a></li>
+                        <li><a href="#">Clients</a></li>
+                        <li><a href="#">Self</a></li>
+                    </ul>
+                </li>
+                <li class="menu">RESOURCES ▾
+                    <ul>
+                        <li><a href="#">Assets</a></li>
+                        <li><a href="#">Books</a></li>
+                        <li><a href="#">Learning</a></li>
+                        <li><a href="#">Tools</a></li>
+                    </ul>
+                </li>
+                <li class="menu">FEATURES ▾
+                    <ul>
+                        <li><a href="#">Interviews</a></li>
+                        <li><a href="#">Studio Tours</a></li>
+                        <li><a href="#">Creative Briefs</a></li>
+                        <li><a href="#">Shopping</a></li>
+                    </ul>
+                </li>
+                <li id="search">
+                    <a href="#"><img src="images/loupe.png" alt="loupe"></a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!--header on smaller screens-->
+    <header class="smallheader">
+        <img src="images/menu.png" id="menu" alt="menu">
+        <div class="logo">
+            <a href="#"><img src="images/logo.svg" alt="logo"></a>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#"><img src="images/loupe.png" alt="loupe" class="display"> </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
